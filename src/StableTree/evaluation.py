49efbcf97ff40a5d43ab490105384bc668b1b504
mishkin <mlgelman@usc.edu>
2025-04-25 17:48:16 -0700

    added common split function

---
+++ src/StableTree/evaluation.py
@@ -0,0 +1,12 @@
+'''
+=====Imports=======
+
+=====structure=====
+-- all functions need access to pareto trees from main.py
+
+'''
+
+'''we record the two most commonly selected features in each of the
+  first three splits, along with their selection frequencies.'''
+def common_pareto_features(split_depth= None, pareto_set=None):
+    return
